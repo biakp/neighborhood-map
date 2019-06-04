27de5ce512de10674175d355a81659daedd645a1
Bea Knabben <42621272+biakp@users.noreply.github.com>
2019-06-04 17:15:16 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css'
+import Map from './components/Map'
+
+class App extends React.PureComponent {
+
+  render() {
+  return (
+    <div className="App">
+    <Map></Map>    
+    </div>
+  ); }
+}
+
+export default App;
